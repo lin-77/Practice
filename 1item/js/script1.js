879805e59825ddc0e716e7cae56d603c9948b1f7
lin-77 <1329475157@qq.com>
2019-11-01 11:41:47 +0800

    新建项目练习

---
+++ 1item/js/script1.js
@@ -0,0 +1,7 @@
+var oBox = document.querySelector('.box');
+
+oBox.onclick = function () {
+    this.style.background = '#ccc';
+    this.style.color = 'blue';
+    this.style.borderRadius = 50 + '%';
+}
