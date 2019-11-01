b3cb7a74e5b2c49f30b3128583204ea63f9bec59
lin-77 <1329475157@qq.com>
2019-11-01 11:45:02 +0800

    修改功能

--- 1item/js/script1.js
+++ 1item/js/script1.js
@@ -5,3 +5,10 @@ oBox.onclick = function () {
     this.style.color = 'blue';
     this.style.borderRadius = 50 + '%';
 }
+
+var oImg = document.querySelector('img');
+
+oImg.onclick = function(){
+    this.style.border = '1px solid #000';
+    this.style.margin = '0 auto';
+}
